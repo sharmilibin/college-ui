4582a095ea8fdd01907e323a736b80e805711785
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-04-19 23:12:14 -0600

    loginPage

---
+++ src/login/style.js
@@ -0,0 +1,32 @@
+<ReactSignupLoginComponent
+  styles={{
+    mainWrapper: { backgroundColor: 'blue' },
+    mainTitle: { color: 'red' },
+    flipper: { transition: '0.1s' },
+    signup: {
+      wrapper: { backgroundColor: 'yellow' },
+      inputWrapper: { backgroundColor: 'AliceBlue' },
+      buttonsWrapper: { backgroundColor: 'Aqua' },
+      input: { backgroundColor: 'LavenderBlush' },
+      recoverPassword: {},
+      button: { backgroundColor: 'LightCoral' },
+    },
+    login: {
+      wrapper: { backgroundColor: 'yellow' },
+      inputWrapper: { backgroundColor: 'AliceBlue' },
+      buttonsWrapper: { backgroundColor: 'Aqua' },
+      input: { backgroundColor: 'LavenderBlush' },
+      recoverPasswordWrapper: { backgroundColor: 'MediumBlue' },
+      recoverPasswordButton: { backgroundColor: 'OldLace ' },
+      button: { backgroundColor: 'LightCoral' },
+    },
+    recoverPassword: {
+      wrapper: { backgroundColor: 'yellow' },
+      inputWrapper: { backgroundColor: 'AliceBlue' },
+      buttonsWrapper: { backgroundColor: 'Aqua' },
+      input: { backgroundColor: 'LavenderBlush' },
+      button: { backgroundColor: 'LightCoral' },
+    },
+  }}
+  
+/>
